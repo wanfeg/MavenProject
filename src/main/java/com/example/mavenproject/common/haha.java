13111b071ec21a40d8a573f90f2a559a578b533c
wanfeg <wanfeng52134@163.com>
2023-08-14 11:07:03 +0800

    增加了haha类

---
+++ src/main/java/com/example/mavenproject/common/haha.java
@@ -0,0 +1,13 @@
+package com.example.mavenproject.common;
+
+/**
+ * @author :Aurora
+ * @version:
+ * @date : 2023/8/14 11:05
+ * @desc:
+ */
+public class haha {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
