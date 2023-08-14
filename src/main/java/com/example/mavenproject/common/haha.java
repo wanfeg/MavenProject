829d41d6a371f8dd082fbb326dacec828f3644ff
wanfeg <wanfeng52134@163.com>
2023-08-14 11:35:15 +0800

    改变haha类，打印线程地址

--- src/main/java/com/example/mavenproject/common/haha.java
+++ src/main/java/com/example/mavenproject/common/haha.java
@@ -9,5 +9,8 @@ package com.example.mavenproject.common;
 public class haha {
     public static void main(String[] args) {
         System.out.println("haha");
+        Thread thread = Thread.currentThread();
+        System.out.println("thread = " + thread);
     }
+
 }
