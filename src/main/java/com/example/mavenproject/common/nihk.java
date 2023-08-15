e9c43ae76c7b86b318a35245024f51664b5566e2
wanfeg <wanfeng52134@163.com>
2023-08-15 14:45:14 +0800

    增加nihk

---
+++ src/main/java/com/example/mavenproject/common/nihk.java
@@ -0,0 +1,13 @@
+package com.example.mavenproject.common;
+
+/**
+ * @author :Aurora
+ * @version:
+ * @date : 2023/8/15 14:44
+ * @desc:
+ */
+public class nihk {
+    public static void main(String[] args) {
+
+    }
+}
