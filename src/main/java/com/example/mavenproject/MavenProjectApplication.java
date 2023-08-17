2f27082bbbd6b8c2b1912dc2c80c64fa922f82ce
wanfeg <wanfeng52134@163.com>
2023-08-17 10:22:29 +0800

    关于动态代理的代码

--- src/main/java/com/example/mavenproject/MavenProjectApplication.java
+++ src/main/java/com/example/mavenproject/MavenProjectApplication.java
@@ -2,7 +2,10 @@ package com.example.mavenproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+
+@EnableAsync
 @SpringBootApplication
 public class MavenProjectApplication {
 
